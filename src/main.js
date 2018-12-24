97e02d16770ed56b93f71a68771fd18fc8167676
Werner1201 <werner.romling@unigranrio.br>
2018-12-24 16:41:13 -0200

    Configs Comuns

---
+++ src/main.js
@@ -0,0 +1,15 @@
+//Axios
+import axios from 'axios';
+
+class Api {
+    static async getUserInfo(username){
+        try {
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+        }catch(err){
+            console.warn('Erro na API');
+        } 
+        console.log(response);
+    }
+}
+
+Api.getUserInfo('Werner12f01');
