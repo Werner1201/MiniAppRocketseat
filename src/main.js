ba85f5ae22699c4d408c86b98db62e65a43fb04a
Werner1201 <werner.romling@unigranrio.br>
2018-12-25 11:34:59 -0200

    Manejo de Erros e Carregamento e Comentários

--- src/main.js
+++ src/main.js
@@ -9,33 +9,85 @@ class App {
         this.manejoRegistros();
     }
 
-    manejoRegistros(){
+    manejoRegistros() {
         this.eleForm.onsubmit = evento => this.addRepositorio(evento);
     }
 
-    async addRepositorio(evento){
+    //Carregando se sim ou nao
+    setCarregando(carregando = true) {
+        if (carregando === true) {
+            //Break Line e Escrito Carregando
+            let brkLine = document.createElement('br');
+            let elemCarregando = document.createElement('span');
+            //Define Id de Span para remove-lo depois
+            elemCarregando.setAttribute('id', 'carregando');
+            //Escreve Carregando
+            elemCarregando.appendChild(document.createTextNode('Carregando'));
+
+            //Codigo da Internet para fazer 3 pontinhos carregando
+            window.dotsGoingUp = true;
+            var dots = window.setInterval(function () {
+                var wait = document.getElementById('carregando');
+                if (window.dotsGoingUp)
+                    wait.innerHTML += ".";
+                else {
+                    wait.innerHTML = wait.innerHTML.substring(1, wait.innerHTML.length);
+                    if (wait.innerHTML === "")
+                        window.dotsGoingUp = true;
+                }
+                if (wait.innerHTML.length > 13)
+                    window.dotsGoingUp = false;
+            }, 100);
+            //Acrescenta o Carregando ao Formulario
+            this.eleForm.appendChild(elemCarregando);
+        } else {
+            //Remove o carregando caso a condicao seja falsa
+            document.getElementById('carregando').remove();
+        }
+    }
+    //Adciona Repositorio
+    async addRepositorio(evento) {
         evento.preventDefault();
 
         const valorInput = this.eleInput.value;
-        if(valorInput.lenght === 0)
+        if (valorInput.lenght === 0)
             return;
 
-        const response = await api.get(`/repos/${valorInput}`);
-        
-        const {name, description, html_url, owner: {avatar_url} } = response.data;
+        this.setCarregando();
+        //Tentativa de Requisicao
+        try {
+            const response = await api.get(`/repos/${valorInput}`);
 
-        this.repositorios.push({
-            nome: name,
-            descricao: description,
-            avatar_url: avatar_url,
-            html_url: html_url
-        });
-        this.eleInput.value = '';
+            const {
+                name,
+                description,
+                html_url,
+                owner: {
+                    avatar_url
+                }
+            } = response.data;
 
-        this.renderiza();
+            this.repositorios.push({
+                nome: name,
+                descricao: description,
+                avatar_url: avatar_url,
+                html_url: html_url
+            });
+
+            //Apaga os valores de dentro do input qnd faz a requisicao
+            this.eleInput.value = '';
+
+            //Renderiza as Informacoes
+            this.renderiza();
+        } catch (err) {
+            //Alerta caso a requisicao volte vazia ou com erro.
+            alert('O repositório não existe!');
+        }
+        //Define a condicao falsa para remover o elemento de carregando.
+        this.setCarregando(false);
     }
 
-    renderiza(){
+    renderiza() {
         this.eleBaseLista.innerHTML = '';
         this.repositorios.forEach(repo => {
             //Cria a Imagem 
