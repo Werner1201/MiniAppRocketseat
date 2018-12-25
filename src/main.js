cb066b9482df740eb559e6730688d99991d52ec2
Werner1201 <werner.romling@unigranrio.br>
2018-12-25 01:00:18 -0200

    Renderização ao Adicionar um Repositório

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ class App {
         this.repositorios = [];
 
         this.eleForm = document.getElementById('repo-form');
+        this.eleBaseLista = document.getElementById('repo-list');
         this.manejoRegistros();
     }
 
@@ -19,9 +20,42 @@ class App {
             avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
             html_url: 'http://github.com/rocketseat/'
         });
-        console.log(this.repositorios);
+        
+        this.renderiza();
     }
 
+    renderiza(){
+        this.eleBaseLista.innerHTML = '';
+        this.repositorios.forEach(repo => {
+            //Cria a Imagem 
+            let eleImg = document.createElement('img');
+            eleImg.setAttribute('src', repo.avatar_url);
+
+            //Cria o Nome
+            let eleNome = document.createElement('strong');
+            eleNome.appendChild(document.createTextNode(repo.nome));
+
+            //Cria a descricao
+            let eleDesc = document.createElement('p');
+            eleDesc.appendChild(document.createTextNode(repo.descricao));
+
+            //Cria o Link 
+            let eleLink = document.createElement('a');
+            eleLink.setAttribute('target', '_blank');
+            eleLink.setAttribute('href', repo.html_url);
+            eleLink.appendChild(document.createTextNode('Acessar'));
+
+            //Elementos da Lista
+            let eleItemLista = document.createElement('li');
+            eleItemLista.appendChild(eleImg);
+            eleItemLista.appendChild(eleNome);
+            eleItemLista.appendChild(eleDesc);
+            eleItemLista.appendChild(eleLink);
+
+            //Colocar os Itens da Lista na Base da Lista
+            this.eleBaseLista.appendChild(eleItemLista);
+        });
+    }
 
 };
 
